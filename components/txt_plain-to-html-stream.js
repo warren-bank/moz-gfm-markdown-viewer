23eb2398634fbdfd36e37fa6045b83b669e4620d
Warren R Bank <warren.r.bank@gmail.com>
2014-08-06 00:38:10 -0700

    refactored all of the "nsIStreamConverter" components
    all "nsIStreamConverter" components are now generated
    from a common factory function.

--- components/txt_plain-to-html-stream.js
+++ components/txt_plain-to-html-stream.js
@@ -1,72 +1,18 @@
-if (!GFM_MarkdownViewer)
-	var GFM_MarkdownViewer = {};
+Components.utils.import("chrome://gfm-markdown-viewer/content/generate_stream_converter.js");
 
-if (!GFM_MarkdownViewer.StreamConverter) {
-	Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+var this_stream_converter = (function(){
+	var class_description, class_id, from_mime_type;
 
-	GFM_MarkdownViewer.StreamConverter = function() {};
+	class_description	= "GitHub Flavored Markdown (GFM) to HTML stream converter";
+	class_id			= "{bb2d3abf-80a3-4355-832c-fa9249a2db24}";
+	from_mime_type		= "text/plain";
 
-	GFM_MarkdownViewer.StreamConverter.prototype = {
-		classDescription: "GitHub Flavored Markdown (GFM) to HTML stream converter",
-		classID: Components.ID("{bb2d3abf-80a3-4355-832c-fa9249a2db24}"),
-		contractID: "@mozilla.org/streamconv;1?from=text/plain&to=*/*",
+	generate_stream_converter(class_description, class_id, from_mime_type);
 
-		_xpcom_factory: {
-			createInstance: function(outer, iid) {
-				if (outer != null)
-					throw Components.results.NS_ERROR_NO_AGGREGATION;
-
-				if (iid.equals(Components.interfaces.nsISupports) ||
-					iid.equals(Components.interfaces.nsIStreamConverter) ||
-					iid.equals(Components.interfaces.nsIStreamListener) ||
-					iid.equals(Components.interfaces.nsIRequestObserver)) {
-					return new GFM_MarkdownViewer.StreamConverter();
-				}
-				throw Components.results.NS_ERROR_NO_INTERFACE;
-			}
-		},
-
-		QueryInterface: XPCOMUtils.generateQI(
-			[Components.interfaces.nsIObserver,
-			Components.interfaces.nsIStreamConverter,
-			Components.interfaces.nsIStreamListener,
-			Components.interfaces.nsIRequestObserver]
-		),
-
-		onStartRequest: function(aRequest, aContext) {
-			this.data    = "";
-			this.uri     = aRequest.QueryInterface (Components.interfaces.nsIChannel).URI.spec;
-		    this.channel = aRequest;
-		    this.channel.contentType = "text/html";
-			this.channel.contentCharset = "UTF-8";
-		    this.listener.onStartRequest (this.channel, aContext);
-		},
-
-		onStopRequest: function(aRequest, aContext, aStatusCode) {
-		    var sis = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(Components.interfaces.nsIStringInputStream);
-		    sis.setData(this.data, this.data.length);
-		    this.listener.onDataAvailable(this.channel, aContext, sis, 0, this.data.length);
-		    this.listener.onStopRequest(this.channel, aContext, aStatusCode);
-		},
-
-		onDataAvailable: function(aRequest, aContext, aInputStream, aOffset, aCount) {
-			var si = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance();
-			si     = si.QueryInterface(Components.interfaces.nsIScriptableInputStream);
-			si.init(aInputStream);
-			this.data += si.read(aCount);
-		},
-
-		asyncConvertData: function(aFromType, aToType, aListener, aCtxt) {
-		    this.listener = aListener;
-		},
-
-		convert: function(aFromStream, aFromType, aToType, aCtxt) {
-		    return aFromStream;
-		}
-	};
-}
+	return GFM_MarkdownViewer.StreamConverters[class_id];
+})();
 
 if (XPCOMUtils.generateNSGetFactory)
-    var NSGetFactory = XPCOMUtils.generateNSGetFactory([GFM_MarkdownViewer.StreamConverter]);
+    var NSGetFactory = XPCOMUtils.generateNSGetFactory([ this_stream_converter ]);
 else
-    var NSGetModule = XPCOMUtils.generateNSGetModule([GFM_MarkdownViewer.StreamConverter]);
+    var NSGetModule  = XPCOMUtils.generateNSGetModule( [ this_stream_converter ]);
